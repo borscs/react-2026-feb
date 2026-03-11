827214fee03d69e0b30a3468227cf58d4057eaa0
borscs <andras.borscs@opticruiter.com>
2026-03-11 17:38:11 +0100

    lesson 4 tic-tac and refs Protals

---
+++ lesson-4/src/App.jsx
@@ -0,0 +1,19 @@
+import Player from './components/Player.jsx';
+import TimerChallenge from "./components/TimerChallenger.jsx";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallenge title={'Easy'} targetTime={1} />
+        <TimerChallenge title={'Not Easy'} targetTime={5} />
+        <TimerChallenge title={'Hard'} targetTime={10} />
+        <TimerChallenge title={'Very Hard'} targetTime={15} />
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
