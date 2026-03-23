6f90f9a54b917a075d2e8552b0deb8b722357325
borscs <andras.borscs@opticruiter.com>
2026-03-23 17:00:50 +0100

    lesson 7

---
+++ lesson-7/src/components/DeleteConfirmation.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from 'react';
+
+const TIMER = 3000;
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+	const [remainingTimer, setRemainingTimer] = useState(TIMER)
+	
+	useEffect(() => {
+		const interval = setInterval(() => {
+			console.log('TIMER');
+			setRemainingTimer(prevTimer => prevTimer - 10)
+		}, 10);
+		return () => {
+			clearInterval(interval);
+		}}, []);
+	
+
+	useEffect(() => {
+		console.log('TIMER SET');
+		const timer = setTimeout(() => {
+			onConfirm();
+		}, TIMER);
+		
+		return () => {
+			console.log('Cleaning up timer');
+			clearTimeout(timer);
+		};
+	}, [onConfirm]);
+	
+	return (
+		<div id="delete-confirmation">
+			<h2>Are you sure?</h2>
+			<p>Do you really want to remove this place?</p>
+			<div id="confirmation-actions">
+				<button onClick={onCancel} className="button-text">
+					No
+				</button>
+				<button onClick={onConfirm} className="button">
+					Yes
+				</button>
+			</div>
+			<progress value={remainingTimer} max={TIMER}/>
+		</div>
+	);
+}
